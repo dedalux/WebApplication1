ba55b156073a56e0f77329620abdc736c0754a16
Kunyi Li <kli9206@gmail.com>
2016-03-31 11:16:36 +0200

    First commit

---
+++ WebApplication1/Scripts/Scripts.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope) {
+                    var employee = {
+                        firstName: "James",
+                        lastName: "Smith"
+                    };
+
+                    $scope.employee = employee
+                });
