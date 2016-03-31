acd14499d3129b07a87e85696734f65c6ede2f26
Kunyi Li <kli9206@gmail.com>
2016-03-31 16:25:54 +0200

    up to lesson 16

--- WebApplication1/Scripts/Scripts.js
+++ WebApplication1/Scripts/Scripts.js
@@ -3,10 +3,56 @@
 var myApp = angular
                 .module("myModule", [])
                 .controller("myController", function ($scope) {
-                    var employee = {
-                        firstName: "James",
-                        lastName: "Smith"
+                    var employees = [
+                            { firstName: "Bob", lastName: "Smith", birthday: new Date("June 20, 1934"), BMI: 20, up: 0, down: 0 },
+                            { firstName: "Carl", lastName: "Tanner", birthday: new Date("April 20, 1946"), BMI: 17, up: 0, down: 0 },
+                            { firstName: "Dean", lastName: "Baker", birthday: new Date("December 17, 1982"), BMI: 35, up: 0, down: 0 },
+                            { firstName: "Grant", lastName: "Fletcher", birthday: new Date("September 17, 1992"), BMI: 27, up: 0, down: 0 },
+                            {
+                                firstName: "Hughe", lastName: "Mason", birthday: new Date("January 17, 2001"), BMI: 25, up: 0, down: 0,
+                                children: [
+                                    { name: "Bobby" },
+                                    { name: "Sally" }
+                                ]
+                            }
+
+                    ];
+
+                    $scope.employees = employees;
+                    $scope.employeesCount = employees.length;
+
+                    $scope.tickUp = function (employee) {
+                        employee.up++;
                     };
 
-                    $scope.employee = employee
+                    $scope.tickDown = function (employee) {
+                        employee.down--;
+                    };
+
+                    // Default input values
+                    $scope.rowLimit = 5;
+                    $scope.sortColumn = "firstName";
+
+                    //
+                    // Video 10 click sort implementation
+                    $scope.reverseSort = false;
+
+                    // Set sort column
+                    $scope.sortData = function (column) {
+                        $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false; // if same column reverse, else set to false
+                        $scope.sortColumn = column; // set sortColumn to column
+                    }
+
+                    // Set up and down arrow based on current sort feature
+                    $scope.getSortClass = function (column) {
+                        if ($scope.sortColumn == column) {
+                            return $scope.reverseSort ? "arrow-down" : "arrow-up";
+                        };
+                    };
+
+                    // Video 15 ng-init
+                    $scope.hideKids = true;
+
+                    // Video 16 ng-include html component
+                    $scope.employeeView = "EmployeeView.html"
                 });
